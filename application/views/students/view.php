9536664c38ef2de1fcd7cebb34ff1eafd780c1ed
Nabihah Taquddin <nabihah.student@gmail.com>
2020-04-24 15:26:51 +0800

    change to table view

--- application/views/students/view.php
+++ application/views/students/view.php
@@ -1,14 +1,26 @@
 <h2><?php echo $student['name']; ?></h2>
-<small class="student-date">Posted on: <?php echo $student['created_at']; ?></small><br>
-<div class="student-age">
 
-    <?php echo $student['age']; ?>
-</div>
-<div class="student-year">
-    <?php echo $student['year']; ?>
-</div>
-<hr>
-<a class="btn btn-default pull-left" href="<?php echo base_url(); ?>students/edit/<?php echo $student['slug']; ?>">Edit</a>
-<?php echo form_open('/students/delete/'.$student['id']); ?>
-    <input type="submit" value="Delete" class="btn btn-danger">
+<div class="form-group">
+    <table class="table table-hover table-dark">
+        <tr>
+            <th>Name</th>
+            <td>: <?php echo $student['name']; ?></td>
+        </tr>
+        <tr>
+            <th>Age</th>
+            <td>: <?php echo $student['age']; ?></td>
+        </tr>
+        <tr>
+            <th>Year</th>
+            <td>: <?php echo $student['year']; ?></td>
+        </tr>
+        <tr>
+            <th>Registered on</th>
+            <td>: <?php echo $student['created_at']; ?></td>
+        </tr>
+    </table>  
+    <hr>
+    <a class="btn btn-default pull-left" href="<?php echo base_url(); ?>students/edit/<?php echo $student['slug']; ?>">Edit</a>
+    <?php echo form_open('/students/delete/'.$student['id']); ?>
+        <input type="submit" value="Delete" class="btn btn-danger">
 </form>
